b09b63021e365b106979f233b333f854e7eb0c8c
= <=>
2025-09-03 21:49:56 +0300

    fixed error of the data process code

--- Expense-categoirzer-budget-predictor/src/utilities.py
+++ Expense-categoirzer-budget-predictor/src/utilities.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-def load_transac(filepath: src) -> pd.DataFrame:
+def load_transac(filepath: str) -> pd.DataFrame:
     df = pd.read_csv(filepath, parse_dates=["date"])
     df = df.dropna(subset=["description", "amount"])
     df["category"] = df["category"].str.strip().str.title()
