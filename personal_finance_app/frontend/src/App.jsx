138cdc2ed057bddba603222b6d6003bc493c1ac0
= <=>
2026-05-28 19:53:44 +0100

    fix to navbar commit

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "frontend/src/components/Navbar";
 
 function App() {
   return (
