9e5fedfd0ba6e681edc2f78904a8d527c3098559
= <=>
2026-05-28 19:46:33 +0100

    NavBar commmit

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1>StudlyFinance</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h1>Dashboard</h1>} />
+        <Route path="/expenses" element={<h1>Expenses</h1>} />
+        <Route path="/savings" element={<h1>Savings</h1>} />
+        <Route path="/split-bills" element={<h1>Split Bills</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
