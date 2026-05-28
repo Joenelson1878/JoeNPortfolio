0998b28cbddf43e70b6dec01b5a4f514695f3cee
= <=>
2026-05-28 19:06:03 +0100

    second navbar commit

--- personal_finance_app/frontend/src/components/navigtionbar.jsx
+++ personal_finance_app/frontend/src/components/navigtionbar.jsx
@@ -1,1 +1,11 @@
-import { link } from "react-router-dom":
+import { Link } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <><nav className="flex items-center justify-between px-6 py-4 bg-blue-600 text-white shadow-md"></nav>
+        <div className="txt-xl font-bold">
+            StudlyFinance
+        </div></>
+
+    );
+}
