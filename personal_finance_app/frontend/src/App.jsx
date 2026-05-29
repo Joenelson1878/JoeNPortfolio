3d2ca5e0b0b365eb3c3315fd2eaf903c04ce0190
= <=>
2026-05-29 22:26:35 +0100

    temp commit to fix a bug

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,28 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-
 function App() {
   return (
-    <BrowserRouter>
-
-      <div className="flex bg-gray-100 min-h-screen">
-
-        <Sidebar />
-
-        <div className="flex-1 p-8">
-
-          <Routes>
-            <Route path="/" element={<h1>Dashboard</h1>} />
-            <Route path="/expenses" element={<h1>Expenses</h1>} />
-            <Route path="/savings" element={<h1>Savings</h1>} />
-            <Route path="/split-bills" element={<h1>Split Bills</h1>} />
-          </Routes>
-
-        </div>
-
-      </div>
-
-    </BrowserRouter>
+    <div>
+      <h1>StudlFinance</h1>
+      <p>React is working!</p>
+    </div>
   );
 }
 
