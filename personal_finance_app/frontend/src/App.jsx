25552026e9430ef86a93851186b94b3d28cf7755
= <=>
2026-05-29 23:38:07 +0100

    alterarions commit

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -2,8 +2,6 @@ function App() {
   return (
     <div>
       <h1>StudlyFinance</h1>
-      <p>React is working!</p>
-      <h2>why is my sidebar not working !!!!!!!!!!!!!!!!!!!!!</h2>
       <ul>
         <li>Dashobard</li>
         <li>Expenses</li>
