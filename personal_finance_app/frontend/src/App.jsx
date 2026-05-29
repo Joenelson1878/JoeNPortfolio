03cfdb7323bb4380564e93691bafd743ef06bb20
= <=>
2026-05-29 23:37:41 +0100

    test list for the dashboard

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -4,6 +4,12 @@ function App() {
       <h1>StudlyFinance</h1>
       <p>React is working!</p>
       <h2>why is my sidebar not working !!!!!!!!!!!!!!!!!!!!!</h2>
+      <ul>
+        <li>Dashobard</li>
+        <li>Expenses</li>
+        <li>Savings</li>
+        <li>Split group bills</li>
+      </ul>
     </div>
   );
 }
