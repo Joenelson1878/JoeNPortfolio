da76441ec8d54b3e3ceb6247ce8b579d86988cd4
= <=>
2026-05-29 22:33:38 +0100

    commit of pages begining

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,10 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+
+import Dashboard from "./pages/Dashboard";
+import Expenses from "./pages/Expenses";
+import Savings from "./pages/Savings";
+import SplitBills from "./pages/SplitBills";
+
 function App() {
   return (
-    <div>
-      <h1>StudlyFinance</h1>
-      <p>React is working!</p>
+    <div className="flex">
+      {/* Sidebar stays always visible */}
+      <Sidebar />
+
+      {/* Page content changes here */}
+      <div className="flex-1 p-6 bg-gray-50 min-h-screen">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/savings" element={<Savings />} />
+          <Route path="/split-bills" element={<SplitBills />} />
+        </Routes>
+      </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
---
+++ personal_finance_app/frontend/src/pages/SplitBills.jsx
@@ -0,0 +1,3 @@
+export default function SplitBills() {
+    return <h1 className="text-2xl font-bold">SplitBills</h1>;
+  }
