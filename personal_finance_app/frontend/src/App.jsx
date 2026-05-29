b06d02340cc117c28c87f44e5646735628789dc0
= <=>
2026-05-29 22:41:35 +0100

    test part 2

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,13 +1,8 @@
-import Sidebar from "./components/Sidebar";
-
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-
-      <div className="flex-1 p-6">
-        <h1>App is working</h1>
-      </div>
+    <div>
+      <h1>StudlFinance</h1>
+      <p>React is working!</p>
     </div>
   );
 }
