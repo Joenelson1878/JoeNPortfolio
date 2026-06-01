c755b4741abe34c49c385a1b5288349a1ed7d25d
= <=>
2026-06-01 20:58:39 +0100

    hero section homepage commit

---
+++ personal_finance_app/frontend/src/components/hero.jsx
@@ -0,0 +1,13 @@
+export default function hero() {
+    return (
+        <section className="hero">
+            <h1>Take control of your money</h1>
+            <p>Track spending, manage savings, and split bills effortlessly using StudlyFiance</p>
+
+            <div className="buttons">
+                <button className="primary"> Get Started</button>
+                <button className="secondary">View Demo</button>
+            </div>
+        </section>
+    );
+}
