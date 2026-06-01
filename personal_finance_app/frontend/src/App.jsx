9403dd7e6bbfa80370f66e23746e646ea2b8122d
= <=>
2026-06-01 17:12:44 +0100

    navbar addition commit

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,14 +1,14 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <div>
-      <h1>StudlyFinance</h1>
-      <ul>
-        <li>Dashobard</li>
-        <li>Expenses</li>
-        <li>Savings</li>
-        <li>Split group bills</li>
-      </ul>
-    </div>
+    <>
+      <Sidebar />
+
+      <main>
+        <h1>Welcome to StudlyFinance</h1>
+      </main>
+    </>
   );
 }
 
