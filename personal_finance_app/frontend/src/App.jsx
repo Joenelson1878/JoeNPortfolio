f107c2925d5c70fe4472163e707ba789be5b6216
= <=>
2026-06-01 23:03:41 +0100

    test commit

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar";
 import hero from "./components/hero";
+import Expenses from "./pages/Expenses";
 function App() {
   return (
     <div>
       <Sidebar />
-      <hero />   
+      <hero />
+      <Expenses />
     </div>
   );
 }
