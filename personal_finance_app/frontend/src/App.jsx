8ab8828f15c2ded8e9f7b04f013ad84f4ca31a4d
= <=>
2026-06-01 21:00:28 +0100

    adding hero to app.jsx commit

--- personal_finance_app/frontend/src/App.jsx
+++ personal_finance_app/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "./components/Sidebar";
+import hero from "./components/hero";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <hero />
       <main>
         <h1>Welcome to StudlyFinance</h1>
       </main>
